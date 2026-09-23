807592f53ec4a60b0e5aca2e83c7a4a122e05f1c
Mohd Faizan Khan <amramoin2@gmail.com>
2026-09-24 00:34:24 +0530

    chore: bump version to 0.1.1

--- envguard/__init__.py
+++ envguard/__init__.py
@@ -1,6 +1,6 @@
 """envguard - Environment variable audit and validation toolkit."""
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 from .models import EnvEntry, EnvFile, DiffResult, ValidationIssue, ValidationResult
 from .parser import parse_env_file, parse_env_content
